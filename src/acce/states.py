dc33c402c8e19585bdf959bb4382b39269cbf69b
jarretwilley <jarret.willey@gmail.com>
2026-03-29 21:33:11 -0400

    Add state definitions

---
+++ src/acce/states.py
@@ -0,0 +1,9 @@
+from enum import Enum, auto
+
+class SystemState(Enum):
+    INIT = auto()
+    STRUCTURE = auto()
+    BOND = auto()
+    BUFFER = auto()
+    CATALYZE = auto()
+    CONTROL = auto()
